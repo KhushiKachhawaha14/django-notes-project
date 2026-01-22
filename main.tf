41b2c7e21e3a3684fea2443cebb20d01f8dded56
Khushi Kachhawaha <khushikachwaha26@gmail.com>
2026-01-22 19:11:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,18 +9,38 @@ terraform {
 
 provider "docker" {}
 
-# Pull the Nginx image
-resource "docker_image" "nginx_image" {
-  name         = "nginx:latest"
-  keep_locally = false
+# 1. Create a Network so containers can talk to each other
+resource "docker_network" "app_network" {
+  name = "notes_network"
 }
 
-# Create the Nginx container
-resource "docker_container" "nginx_container" {
-  image = docker_image.nginx_image.image_id
-  name  = "notes_app_server"
+# 2. MySQL Database Container
+resource "docker_image" "mysql_image" {
+  name = "mysql:8.0"
+}
+
+resource "docker_container" "db" {
+  name  = "mysql_db"
+  image = docker_image.mysql_image.image_id
+  networks_advanced { name = docker_network.app_network.name }
+  env = [
+    "MYSQL_ROOT_PASSWORD=password",
+    "MYSQL_DATABASE=notes_db"
+  ]
+}
+
+# 3. Django Backend Container
+resource "docker_image" "django_app" {
+  name = "django-notes-app:latest" # Ensure this matches your local build tag
+}
+
+resource "docker_container" "backend" {
+  name  = "django_backend"
+  image = docker_image.django_app.image_id
+  networks_advanced { name = docker_network.app_network.name }
   ports {
-    internal = 80
-    external = 8000
+    internal = 8000
+    external = 8001
   }
+  depends_on = [docker_container.db]
 }
